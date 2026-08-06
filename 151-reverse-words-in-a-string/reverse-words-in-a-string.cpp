145ad1840bf0b01452a077209e511732536151a2
skshivam1486-create <skshivam1486@gmail.com>
2026-08-07 01:35:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (62.73%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(),s.end());
+        int n=s.length();
+        string ans="";
+
+        for(int i=0;i<n;i++){
+            string words="";
+
+            while(i<n && s[i] != ' '){
+                words +=s[i];
+                i++;
+            }
+            reverse(words.begin(),words.end());
+            if(words.length()>0){
+                ans +=" "+ words;
+            }
+
+        }
+        return ans.substr(1);
+        
+    }
+};
