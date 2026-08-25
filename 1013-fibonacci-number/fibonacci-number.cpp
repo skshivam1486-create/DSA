cef98a1383d3674eeccf1d603e5a6eacc56d675d
skshivam1486-create <skshivam1486@gmail.com>
2026-08-25 23:11:45 +0530

    Time: 14 ms (22.48%) | Memory: 7.7 MB (65.67%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,10 +3,10 @@ public:
     int fib(int n) {
         if(n==0 || n==1){
             return n;
-        }else{
+        }
     
             return fib(n-1)+fib(n-2);
-        }
+        
         
     }
 };
